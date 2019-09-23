3f98230bcde632a353f915a2342578f6854a258b
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2019-09-23 11:54:03 +0200

    Actor network specified.

--- model.py
+++ model.py
@@ -6,6 +6,27 @@ import torch.nn.functional as f
 
 
 class Actor(nn.Module):
+    def __init__(self, state_size, action_size, fc1=256, fc2=128, leak=0.01, seed=42):
+        super(Actor, self).__init__()
+        self.leak = leak
+        self.seed = torch.manual_seed(seed)
+
+        self.fc1 = nn.Linear(state_size, fc1)
+        self.fc2 = nn.Linear(fc1, fc2)
+        self.fc3 = nn.Linear(fc2, action_size)
+
+    def forward(self, state):
+        state = self.bn(state)
+        x = f.leaky_relu(self.fc1(state), negative_slope=self.leak)
+        x = f.leaky_relu(self.fc2(x), negative_slope=self.leak)
+        x = torch.tanh(self.fc3(x))
+        return x
+
+
+class Critic(nn.Module):
+    def __init__(self, state_size, action_size, fc1=256, fc2=128, fc3=128, leak=0.01, seed=42):
+        super(Critic, self).__init__()
+
     def forward(self, *input):
         pass
 
