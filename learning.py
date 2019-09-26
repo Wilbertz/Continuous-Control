186a5d2a5a1472679cb4bef618d299e9f1f5c0f5
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2019-09-26 10:43:09 +0200

    Model completed.

--- learning.py
+++ learning.py
@@ -1,4 +1,3 @@
-from unityagents import UnityEnvironment
 import numpy as np
 
 from collections import deque
@@ -11,4 +10,37 @@ import torch.optim as optim
 
 from agent import Agent
 
-env = UnityEnvironment(file_name='Reacher_Linux_NoVis/Reacher.x86_64')
+def ddpg(n_episodes=2000, max_t=1000):
+    scores_deque = deque(maxlen=100)
+    scores = []
+    max_score = -np.Inf
+    for i_episode in range(1, n_episodes + 1):
+        env_info = env.reset(train_mode=True)[brain_name]
+        states = env_info.vector_observations
+
+        agent.reset()
+        episode_scores = np.zeros(num_agents)
+
+        for t in range(max_t):
+            actions = agent.act(states)
+            env_info = env.step(actions)[brain_name]
+            next_states = env_info.vector_observations
+            rewards = env_info.rewards
+            dones = env_info.local_done
+
+            agent.step(t + 1, states, actions, rewards, next_states, dones)
+            states = next_states
+            episode_scores += rewards
+            if np.any(dones):
+                break
+
+        score = episode_scores.mean(axis=0)
+        scores_deque.append(score)
+        scores.append(score)
+        print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, np.mean(scores_deque), score),
+              end="")
+        if i_episode % 100 == 0:
+            torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
+            torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))
+    return scores
--- model.py
+++ model.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as f
@@ -14,6 +12,8 @@ class Actor(nn.Module):
         self.fc1 = nn.Linear(state_size, fc1)
         self.fc2 = nn.Linear(fc1, fc2)
         self.fc3 = nn.Linear(fc2, action_size)
+        self.bn = nn.BatchNorm1d(state_size)
+        self.reset_parameters()
 
     def forward(self, state):
         state = self.bn(state)
@@ -22,11 +22,38 @@ class Actor(nn.Module):
         x = torch.tanh(self.fc3(x))
         return x
 
+    def reset_parameters(self):
+        """ Initialize the weights using He et al (2015) weights """
+        torch.nn.init.kaiming_normal_(self.fc1.weight.data, a=self.leak, mode='fan_in')
+        torch.nn.init.kaiming_normal_(self.fc2.weight.data, a=self.leak, mode='fan_in')
+        torch.nn.init.uniform_(self.fc3.weight.data, -3e-3, 3e-3)
+
 
 class Critic(nn.Module):
     def __init__(self, state_size, action_size, fc1=256, fc2=128, fc3=128, leak=0.01, seed=42):
         super(Critic, self).__init__()
+        self.leak = leak
+        self.seed = torch.manual_seed(seed)
+        self.bn = nn.BatchNorm1d(state_size)
+        self.fcs1 = nn.Linear(state_size, fc1)
+        self.fc2 = nn.Linear(fc1 + action_size, fc2)
+        self.fc3 = nn.Linear(fc2, fc3)
+        self.fc4 = nn.Linear(fc3, 1)
+        self.reset_parameters()
 
-    def forward(self, state):
-        pass
+    def reset_parameters(self):
+        """ Initialize all the weights using He et al (2015)."""
+        torch.nn.init.kaiming_normal_(self.fcs1.weight.data, a=self.leak, mode='fan_in')
+        torch.nn.init.kaiming_normal_(self.fc2.weight.data, a=self.leak, mode='fan_in')
+        torch.nn.init.uniform_(self.fc3.weight.data, -3e-3, 3e-3)
+
+    def forward(self, state, action):
+        """ Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        state = self.bn(state)
+        x = f.leaky_relu(self.fcs1(state), negative_slope=self.leak)
+        x = torch.cat((x, action), dim=1)
+        x = f.leaky_relu(self.fc2(x), negative_slope=self.leak)
+        x = f.leaky_relu(self.fc3(x), negative_slope=self.leak)
+        x = self.fc4(x)
+        return x
 
