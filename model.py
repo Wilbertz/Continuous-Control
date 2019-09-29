281d19cc66d943ec41eea214a2d06e28467f0942
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2019-09-29 12:40:17 +0200

    Return values specified.

--- model.py
+++ model.py
@@ -5,7 +5,8 @@ import torch.nn as nn
 import torch.nn.functional as f
 
 
-def hidden_init(layer):
+def hidden_init(layer) -> None:
+
     fan_in = layer.weight.data.size()[0]
     lim = 1. / np.sqrt(fan_in)
     return -lim, lim
@@ -14,7 +15,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300):
+    def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300) -> None:
         """Initialize parameters and build model.
         Params
         ======
@@ -27,14 +28,14 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc1_units)
-        
         self.bn1 = nn.BatchNorm1d(fc1_units)
         
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
+
         self.reset_parameters()
 
-    def reset_parameters(self):
+    def reset_parameters(self) -> None:
         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
@@ -42,7 +43,6 @@ class Actor(nn.Module):
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
         x = f.relu(self.bn1(self.fc1(state)))
-        
         x = f.relu(self.fc2(x))
         return f.tanh(self.fc3(x))
 
@@ -50,7 +50,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=400, fc2_units=300):
+    def __init__(self, state_size, action_size, seed, fcs1_units=400, fc2_units=300) -> None:
         """Initialize parameters and build model.
         Params
         ======
@@ -71,7 +71,7 @@ class Critic(nn.Module):
         self.fc3 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
 
-    def reset_parameters(self):
+    def reset_parameters(self) -> None:
         self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
