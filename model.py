61a37a8b455699f7b85789ab004eed8825f713bb
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2019-10-04 11:57:30 +0200

    Comments for actor network improved.

--- model.py
+++ model.py
@@ -5,6 +5,10 @@ import torch.nn as nn
 import torch.nn.functional as f
 
 
+WEIGHT_MAX = 3e-3
+WEIGHT_MIN = -1.0 * WEIGHT_MAX
+
+
 def hidden_init(layer):
     """"
         Method used by both the Actor and Critic to initialize the hidden layer.
@@ -27,14 +31,14 @@ class Actor(nn.Module):
     def __init__(self, state_size: int, action_size: int, seed: int, fc1_units: int = 400, fc2_units: int = 300) \
             -> None:
         """
-           Initialize fields and build the model. The architecture of the network is based on the parameters
-           given in  "Continuous control with deep reinforcement learning" (https://arxiv.org/abs/1509.02971).
-                Args:
-                    state_size (int): Dimension of each state
-                    action_size (int): Dimension of each action
-                    seed (int): Random seed
-                    fc1_units (int): Number of nodes in first hidden layer
-                    fc2_units (int): Number of nodes in second hidden layer
+            Initialize fields and build the model. The architecture of the network is based on the parameters
+            given in  "Continuous control with deep reinforcement learning" (https://arxiv.org/abs/1509.02971).
+            Args:
+                state_size (int): Dimension of each state
+                action_size (int): Dimension of each action
+                seed (int): Random seed
+                fc1_units (int): Number of nodes in first hidden layer
+                fc2_units (int): Number of nodes in second hidden layer
 
         """
         super(Actor, self).__init__()
@@ -53,11 +57,11 @@ class Actor(nn.Module):
         """
         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc3.weight.data.uniform_(WEIGHT_MIN, WEIGHT_MAX)
 
     def forward(self, state):
         """
-            Forward propagation of input.
+            Forward propagation of input in order to predict actions.
             Args:
                 state (PyTorch model): The observed state used to predict actions
             Returns:
@@ -69,39 +73,46 @@ class Actor(nn.Module):
 
 
 class Critic(nn.Module):
-    """Critic (Value) Model."""
+    """ Critic Model."""
 
-    def __init__(self, state_size: int, action_size: int, seed: int, fcs1_units: int = 400, fc2_units: int = 300) \
+    def __init__(self, state_size: int, action_size: int, seed: int, fc1_units: int = 400, fc2_units: int = 300) \
             -> None:
-        """Initialize parameters and build model. The architecture of the network is based on the parameters
-        given in  "Continuous control with deep reinforcement learning" (https://arxiv.org/abs/1509.02971).
-        Params: int
-        ======
-            state_size (int): Dimension of state vector.
-            action_size (int): Dimension of action vector.
-            seed (int): Used to initialize random number generator.
-            fcs1_units (int): Number of nodes in hidden layer 1
-            fc2_units (int): Number of nodes in hidden layer 2
+        """
+            Initialize parameters and build model. The architecture of the network is based on the parameters
+            given in  "Continuous control with deep reinforcement learning" (https://arxiv.org/abs/1509.02971).
+            Args:
+                state_size (int): Dimension of each state
+                action_size (int): Dimension of each action
+                seed (int): Random seed
+                fc1_units (int): Number of nodes in first hidden layer
+                fc2_units (int): Number of nodes in second hidden layer
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
         
-        self.fcs1 = nn.Linear(state_size, fcs1_units)
-        
-        self.bn1 = nn.BatchNorm1d(fcs1_units)
-        
-        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
+        self.fcs1 = nn.Linear(state_size, fc1_units)
+        self.bn1 = nn.BatchNorm1d(fc1_units)
+        self.fc2 = nn.Linear(fc1_units + action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
 
     def reset_parameters(self) -> None:
+        """
+            Reset the nodes weights.
+        """
         self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc3.weight.data.uniform_(WEIGHT_MIN, WEIGHT_MAX)
 
     def forward(self, state, action):
-        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-
+        """
+            Forward propagation of input in order to predict actions.
+            Args:
+                state (PyTorch model): The observed state used to predict q values.
+                action (PyTorch model): The selected action used to predict q values.
+            Returns:
+                An single q value.
+        """
         xs = f.relu(self.bn1(self.fcs1(state)))
         
         x = torch.cat((xs, action), dim=1)
